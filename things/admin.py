d2bc81e6ff15e20dab80d0f39b207915af85df94
ghpix <psixomail@gmail.com>
2020-01-31 12:50:04 +0300

    make migrations & config it

--- things/admin.py
+++ things/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Place, Device, Type
+
+admin.site.register(Place)
+admin.site.register(Device)
+admin.site.register(Type)
--- things/apps.py
+++ things/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ThingsConfig(AppConfig):
     name = 'things'
+    verbose_name = 'Devices'
