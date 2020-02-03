3cc64301f732b6bb77b4d0df665eb7df4e5a7711
ghpix <psixomail@gmail.com>
2020-02-03 11:52:03 +0300

    add model, manufacturer add fields on dev

--- things/models.py
+++ things/models.py
@@ -22,6 +22,25 @@ class Type(models.Model):
     type_name = models.CharField(max_length=50)
 
 
+class Manufacturer(models.Model):
+    class Meta:
+        verbose_name = 'Type'
+        verbose_name_plural = 'Types'
+
+    manufacturer_id = models.AutoField(primary_key=True)
+    manufacturer_name = models.CharField(max_length=50)
+
+
+class Model(models.Model):
+    class Meta:
+        verbose_name = 'Type'
+        verbose_name_plural = 'Types'
+
+    model_id = models.AutoField(primary_key=True)
+    model_manufacturer_id = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, )
+    model_name = models.CharField(max_length=50)
+
+
 class Device(models.Model):
     class Meta:
         verbose_name = 'Device'
@@ -29,6 +48,8 @@ class Device(models.Model):
 
     device_id = models.AutoField(primary_key=True)
     device_name = models.CharField(max_length=50)
+    device_service_date = models.DateField()
+    device_model_id = models.ForeignKey(Model, on_delete=models.CASCADE, )
     device_place_id = models.ForeignKey(Place, on_delete=models.CASCADE, )
     device_type_id = models.ForeignKey(Type, on_delete=models.CASCADE, )
     device_refillable = models.BooleanField()
