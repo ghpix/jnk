09e01619041c5b139888c45a31babfe5f0bb1fb5
ghpix <psixomail@gmail.com>
2020-02-03 16:12:27 +0300

    some fix and make serial number maker method

--- things/admin.py
+++ things/admin.py
@@ -5,8 +5,27 @@ from django.contrib import admin
 
 from .models import Place, Device, Type, Manufacturer, Model
 
+'''
+device_id = models.AutoField(primary_key=True)
+    device_name = models.CharField(max_length=50, unique=True)
+    device_serial_number = models.CharField(max_length=50, unique=True, default=datetime.now())
+    device_service_date = models.DateField(default=datetime.now())
+    device_model_id = models.ForeignKey(Model, on_delete=models.CASCADE, default=None)
+    device_place_id = models.ForeignKey(Place, on_delete=models.CASCADE, default=None)
+    device_type_id = models.ForeignKey(Type, on_delete=models.CASCADE, default=None)
+    device_refillable = models.BooleanField()
+'''
+
+
+@admin.register(Device)
+class ThingsAdmin(admin.ModelAdmin):
+    list_display = ('device_id', 'device_name', 'device_serial_number', 'device_service_date',)
+    list_display_links = ('device_id', 'device_name', 'device_serial_number', 'device_service_date',)
+    empty_value_display = 'not stated'
+
+
 admin.site.register(Place)
-admin.site.register(Device)
+# admin.site.register(Device)
 admin.site.register(Type)
 admin.site.register(Manufacturer)
 admin.site.register(Model)
--- things/models.py
+++ things/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime, date
 
 
 # Create your models here.
@@ -10,7 +11,10 @@ class Place(models.Model):
         verbose_name_plural = 'Places'
 
     place_id = models.AutoField(primary_key=True)
-    device_name = models.CharField(max_length=50)
+    place_name = models.CharField(max_length=50, unique=True, default=None)
+
+    def __str__(self):
+        return f'{self.place_name}'
 
 
 class Type(models.Model):
@@ -19,7 +23,10 @@ class Type(models.Model):
         verbose_name_plural = 'Types'
 
     type_id = models.AutoField(primary_key=True)
-    type_name = models.CharField(max_length=50)
+    type_name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f'{self.type_name}'
 
 
 class Manufacturer(models.Model):
@@ -28,7 +35,10 @@ class Manufacturer(models.Model):
         verbose_name_plural = 'Manufacturers'
 
     manufacturer_id = models.AutoField(primary_key=True)
-    manufacturer_name = models.CharField(max_length=50)
+    manufacturer_name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f'{self.manufacturer_name}'
 
 
 class Model(models.Model):
@@ -37,8 +47,11 @@ class Model(models.Model):
         verbose_name_plural = 'Models'
 
     model_id = models.AutoField(primary_key=True)
+    model_name = models.CharField(max_length=50, unique=True)
     model_manufacturer_id = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, )
-    model_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.model_name}'
 
 
 class Device(models.Model):
@@ -46,10 +59,29 @@ class Device(models.Model):
         verbose_name = 'Device'
         verbose_name_plural = 'Devices'
 
+    # _make_sn my company default serial number format DDMMYYNUM   - NUM > number 00-99
+    @staticmethod
+    def _make_sn():
+        date_str = datetime.now().strftime('%d%m%y')
+        obj = Device.objects.filter(device_serial_number__startswith=date_str)
+        today_count = 1 + obj.count()
+        return date_str + "%02d" % today_count
+
+    def get_date_f(self):
+        return date.today().strftime('%D-%M-%Y')
+
+    device_sn = _make_sn.__get__(object)
+
     device_id = models.AutoField(primary_key=True)
-    device_name = models.CharField(max_length=50)
-    device_service_date = models.DateField(default=None)
+    device_name = models.CharField(max_length=50, unique=True)
+    device_serial_number = models.CharField(max_length=50, unique=True, default=device_sn)
+    device_service_date = models.DateField(default=datetime.now())
+    device_add_date = models.DateField(default=datetime.now())
     device_model_id = models.ForeignKey(Model, on_delete=models.CASCADE, default=None)
     device_place_id = models.ForeignKey(Place, on_delete=models.CASCADE, default=None)
     device_type_id = models.ForeignKey(Type, on_delete=models.CASCADE, default=None)
     device_refillable = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.device_id} {self.device_name} {self.device_serial_number}   -  {self.device_service_date}'
+
