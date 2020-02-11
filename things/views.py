b9dec369f1da6fce9691ece71896a5db9169310c
ghpix <psixomail@gmail.com>
2020-02-11 19:08:36 +0300

    refactor base html

--- things/views.py
+++ things/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+from .models import Device
 
 
 @require_http_methods(['GET', 'POST'])
 def things_index(request):
+    device = Device.objects.all()
     args = {
+        'device': device,
         'test': list(range(1, 11)),  # например список 10 чисел
     }
     response = render(request, 'things/index.html', args)
     return response
+
+
