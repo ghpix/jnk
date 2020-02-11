e96507caec0a6a0c86bf125f1e2f38f32c91f762
ghpix <psixomail@gmail.com>
2020-02-11 21:12:48 +0300

    refactor view things start view things details

--- things/urls.py
+++ things/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
+from things.views import *
 
 app_name = 'things'
 
 urlpatterns = [
-    path('', views.things_index, name='index'),
-    # path('$Index_Name_2/', views.$Index_Name_2, name='$Some_Link_Name'),
+    # path('', views.things_index, name='index'),
+    path('', ThingsList.as_view(), name='index'),
+    # path('<int:device_id>', views.things_id, name='index_id'),
+    path('<int:device_id>', ThingsId.as_view(), name='device_details'),
+    # url(r'^(?P<pk>\d+)$', ThingsId.as_view(), name='device_details'),
+
 ]
--- things/views.py
+++ things/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from .models import Device
 
 
@@ -15,3 +17,40 @@ def things_index(request):
     return response
 
 
+def things_id(request, device_id):
+    device = Device.objects.all()
+    args = {
+        'device': device,
+        'test': list(range(1, 11)),  # например список 10 чисел
+    }
+    response = render(request, 'things/device_id.html', args)
+    return response
+
+
+class ThingsList(ListView):
+    model = Device
+    template_name = 'things/index.html'
+    context_object_name = 'device_list'
+
+    # Фильт вывода
+    def get_context_data(self, **kwargs):
+        # context = Device.objects.all()
+        context = super().get_context_data(**kwargs)
+        args = {
+            'block_name': 'Device List',
+        }
+        context.update(args)
+        return context
+
+
+class ThingsId(DetailView):
+    model = Device
+    template_name = 'things/index.html'
+    context_object_name = 'device_list'
+
+    # Фильт вывода
+    def get_context_data(self, **kwargs):
+        # context = Device.objects.all()
+        context = super().get_context_data(**kwargs)
+        context['block_name'] = 'Device details'
+        return context
