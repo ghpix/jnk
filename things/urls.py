833b2cfc479297dcd6cbedb49097419e6c1941d8
ghpix <psixomail@gmail.com>
2020-02-16 01:01:14 +0300

    CRUD final stage

--- things/urls.py
+++ things/urls.py
@@ -9,7 +9,5 @@ urlpatterns = [
     path('', ThingsList.as_view(), name='index'),
     path('create/', ThingsCreate.as_view(), name='device_create'),
     url(r'^(?P<pk>[0-9]+)/$', ThingsView.as_view(), name='device_details'),
-    url(r'^(?P<pk>[0-9]+)/edit/$', ThingsUpdate.as_view(), name='device_details'),
-    # url(r'^(?P<pk>[0-9]+)/$', ThingsId.as_view(), name='device_details'),
 
 ]
--- things/views.py
+++ things/views.py
@@ -8,27 +8,6 @@ from .models import Device
 from .forms import DeviceForm
 
 
-# @require_http_methods(['GET', 'POST'])
-# def things_index(request):
-#     device = Device.objects.all()
-#     args = {
-#         'device': device,
-#         'test': list(range(1, 11)),  # например список 10 чисел
-#     }
-#     response = render(request, 'things/index.html', args)
-#     return response
-#
-#
-# def things_id(request, device_id):
-#     device = Device.objects.all()
-#     args = {
-#         'device': device,
-#         'test': list(range(1, 11)),  # например список 10 чисел
-#     }
-#     response = render(request, 'things/device_details.html', args)
-#     return response
-
-
 # show all devices
 class ThingsList(ListView):
     model = Device
@@ -42,44 +21,38 @@ class ThingsList(ListView):
     #     return queryset
 
 
-# show one device details
-class ThingsView(DetailView):
-    model = Device
-    template_name = 'things/device_details.html'
-
-    # def form_valid(self, form):
-    #     # This method is called when valid form data has been POSTed.
-    #     # It should return an HttpResponse.
-    #     # form.send_email()
-    #     return super().form_valid(form)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['row'] = DeviceForm(instance=kwargs['object'])
-        return context
-    #
-    # def post(self, request, **kwargs):
-    #     print(self.kwargs)
-    #
-    #     # if
-    #     form = DeviceForm(request.POST)
-    #
-    #     if form.is_valid():
-    #         # записать в базу сразу
-    #         # form.device_id =
-    #
-    #         rec = form.save(commit=False)
-    #         rec.device_id = kwargs['pk']
-    #         # print(rec)
-    #         rec.save()
-    #
-    #         # # сначала редактирование, потом записать по событию
-    #         # new_user = form.save(commit=False)
-    #         # new_user.level = 'A'
-    #         # new_user.save()
-    #         red_url = 'things: index' + str(kwargs['pk']) + '/'
-    #         return redirect(red_url)
-    #     return render(request, self.template_name, {'form': form})
+# # show one device details
+# # class ThingsView(DetailView):
+# #     model = Device
+# #     template_name = 'things/device_details.html'
+# #
+# #     queryset = Device.objects.all()
+# #
+# #     def get_object(self):
+# #         obj = super().get_object()
+# #         return obj
+#
+#
+# class ThingsView(FormView):
+#     model = Device
+#     template_name = 'things/device_details.html'
+#     form_class = DeviceForm
+#     # queryset = Device.objects.all()
+#
+#
+#     # def get_queryset(self):
+#     #     return Device.objects.all()
+#     #
+#     # def get_initial(self):
+#     #     initial = super(DeviceForm, self).get_initial()
+#     #     return initial
+#     #
+#     # def get_form_kwargs(self):
+#     #     kwargs = super(DeviceForm, self).get_form_kwargs()
+#     #     kwargs.update({'user': self.request.user})
+#     #
+#     # def form_valid(self, form):
+#     #     return super(DeviceForm, self).form_valid(form)
 
 
 class ThingsCreate(CreateView):
@@ -89,7 +62,7 @@ class ThingsCreate(CreateView):
     template_name = 'things/device_details.html'
 
 
-class ThingsUpdate(UpdateView):
+class ThingsView(UpdateView):
     model = Device
     fields = ['device_name', 'device_service_date', 'device_model_id', 'device_serial_number', 'device_place_id',
               'device_type_id', 'device_add_date', 'device_part']
